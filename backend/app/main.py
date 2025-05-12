79bc8bbfb458aa80cf4a8649d1d7f73cd261102d
DavidPerelman <53565536+DavidPerelman@users.noreply.github.com>
2025-05-12 16:45:57 +0300

    feat: upload polygon file from UI and send to FastAPI

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
 from backend.app.api import export_routes
 from backend.app.api.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
     title="GIS Scraper API",
     description="API לקבלת פוליגון, ביצוע גרידת נתונים והחזרת שכבת תוצאה",
     version="0.1.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # או ["*"] לפיתוח
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
