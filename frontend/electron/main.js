e5ef2db4543326bee174e6d6eabd697546a1bb8e
DavidPerelman <53565536+DavidPerelman@users.noreply.github.com>
2025-05-12 15:19:29 +0300

    init electron + react + vite setup

--- frontend/electron/main.js
+++ frontend/electron/main.js
@@ -1,1 +1,15 @@
-// קובץ ראשי של Electron
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+    },
+  });
+
+  win.loadURL("http://localhost:5173");
+}
+
+app.whenReady().then(createWindow);
